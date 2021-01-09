b097f37b3da1dbe7796974f8dc782df342a28d65
Paarth4546M <72933410+Paarth4546M@users.noreply.github.com>
2021-01-09 09:12:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var boxleft,boxright,boxbottom;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+	boxbottom = Bodies.rectangle(400,650,200,20,{isStatic:true})
+    World.add(world,boxbottom)
+
+	boxleft = Bodies.rectangle(300,610,20,100,{isStatic:true})
+	World.add(world,boxleft)
+
+	boxright = Bodies.rectangle(500,610,20,100,{isStatic:true})
+	World.add(world,boxright)
+	
+	//invisible ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true});
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  Engine.update(engine);
+
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  rectMode(CENTER);
+  
+  fill("red")
+  rect(boxbottom.position.x,boxbottom.position.y,200,20)
+  rect(boxleft.position.x,boxleft.position.y,20,100)
+  rect(boxright.position.x,boxright.position.y,20,100)
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+   Matter.Body.setStatic(packageBody,false)
+    
+  }
+}
+
+
+
